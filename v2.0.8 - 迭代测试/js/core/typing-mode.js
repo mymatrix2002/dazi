72d8ffe587073bfaba244e6016fd4fe3abcce254
mymatrix2002 <mymatrix2002@icloud.com>
2026-06-19 00:47:31 +0800

    更新 typing-mode.js

--- v2.0.8 - 迭代测试/js/core/typing-mode.js
+++ v2.0.8 - 迭代测试/js/core/typing-mode.js
@@ -211,7 +211,7 @@ function runTypingBilingualMode(text){
     setTimeout(() => {
         areaTitleEl.scrollIntoView({
             behavior: 'smooth',
-            block: 'center'
+            block: 'start'
         });
     }, 50);
 }
