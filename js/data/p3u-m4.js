d2fd02a4a8e8e0fa52d33fbe090a63f01ff72670
mymatrix2002 <mymatrix2002@icloud.com>
2026-06-29 00:30:02 +0800

    2

---
+++ js/data/p3u-m4.js
@@ -0,0 +1,116 @@
+// js/data/p3u-m4.js
+// 三年级上册 Module 4: Family
+(function() {
+    'use strict';
+
+    const moduleContent = {
+        'u7': {
+            words: [
+                { en: 'picture', cn: '图片' },
+                { en: 'photo', cn: '照片' },
+                { en: 'family', cn: '家庭' },
+                { en: 'father', cn: '爸爸' },
+                { en: 'mother', cn: '妈妈' },
+                { en: 'brother', cn: '兄弟' },
+                { en: 'sister', cn: '姐妹' },
+                { en: 'grandpa', cn: '爷爷' },
+                { en: 'grandma', cn: '奶奶' },
+                { en: 'uncle', cn: '叔叔' },
+                { en: 'aunt', cn: '阿姨' },
+                { en: 'cousin', cn: '表兄弟姐妹' },
+                { en: 'baby', cn: '宝宝' },
+                { en: 'people', cn: '人' },
+                { en: 'how many', cn: '多少' },
+                { en: 'there', cn: '那里' }
+            ],
+            phrases: [
+                { en: 'a picture of my family', cn: '我的全家福' },
+                { en: 'family photo', cn: '全家福照片' },
+                { en: 'my father', cn: '我的爸爸' },
+                { en: 'my mother', cn: '我的妈妈' },
+                { en: 'my brother', cn: '我的兄弟' },
+                { en: 'my sister', cn: '我的姐妹' },
+                { en: 'how many people', cn: '多少人' },
+                { en: 'in the picture', cn: '在图片里' }
+            ],
+            sentences: [
+                { en: 'This is a picture of my family.', cn: '这是我的全家福。' },
+                { en: 'This is my father.', cn: '这是我爸爸。' },
+                { en: 'This is my mother.', cn: '这是我妈妈。' },
+                { en: 'I have a brother.', cn: '我有一个兄弟。' },
+                { en: 'I have a sister.', cn: '我有一个姐妹。' },
+                { en: 'How many people are there in your family?', cn: '你家有多少人？' },
+                { en: 'There are four people in my family.', cn: '我家有四口人。' },
+                { en: 'I love my family.', cn: '我爱我的家庭。' }
+            ],
+            dialogue: [
+                { speaker: 'Janet', en: 'Look, Jiamin! This is a picture of my family.', cn: '看，家民！这是我的全家福。' },
+                { speaker: 'Jiamin', en: 'Wow! It\'s a nice picture.', cn: '哇！真漂亮的照片。' },
+                { speaker: 'Janet', en: 'This is my father and this is my mother.', cn: '这是我爸爸，这是我妈妈。' },
+                { speaker: 'Jiamin', en: 'Who is the boy?', cn: '那个男孩是谁？' },
+                { speaker: 'Janet', en: 'He is my brother, Tom.', cn: '他是我弟弟，汤姆。' },
+                { speaker: 'Jiamin', en: 'How many people are there in your family?', cn: '你家有多少人？' },
+                { speaker: 'Janet', en: 'There are four people.', cn: '有四口人。' },
+                { speaker: 'Jiamin', en: 'There are three people in my family.', cn: '我家有三口人。' },
+                { speaker: 'Janet', en: 'Do you have a brother or sister?', cn: '你有兄弟姐妹吗？' },
+                { speaker: 'Jiamin', en: 'No, I don\'t. But I have a cute cat!', cn: '不，我没有。但是我有一只可爱的猫！' }
+            ]
+        },
+        'u8': {
+            words: [
+                { en: 'who', cn: '谁' },
+                { en: 'that', cn: '那个' },
+                { en: 'lady', cn: '女士' },
+                { en: 'man', cn: '男士' },
+                { en: 'girl', cn: '女孩' },
+                { en: 'boy', cn: '男孩' },
+                { en: 'woman', cn: '女人' },
+                { en: 'tall', cn: '高的' },
+                { en: 'short', cn: '矮的' },
+                { en: 'thin', cn: '瘦的' },
+                { en: 'strong', cn: '强壮的' },
+                { en: 'pretty', cn: '漂亮的' },
+                { en: 'handsome', cn: '英俊的' },
+                { en: 'young', cn: '年轻的' },
+                { en: 'old', cn: '年老的' },
+                { en: 'cute', cn: '可爱的' }
+            ],
+            phrases: [
+                { en: 'who\'s that lady', cn: '那位女士是谁' },
+                { en: 'that man', cn: '那位男士' },
+                { en: 'tall and thin', cn: '又高又瘦' },
+                { en: 'short and strong', cn: '又矮又壮' },
+                { en: 'very pretty', cn: '非常漂亮' },
+                { en: 'very handsome', cn: '非常英俊' },
+                { en: 'young and cute', cn: '年轻又可爱' },
+                { en: 'the girl in red', cn: '穿红衣服的女孩' }
+            ],
+            sentences: [
+                { en: 'Who\'s that lady?', cn: '那位女士是谁？' },
+                { en: 'She\'s my aunt.', cn: '她是我阿姨。' },
+                { en: 'Who\'s that man?', cn: '那位男士是谁？' },
+                { en: 'He\'s my uncle.', cn: '他是我叔叔。' },
+                { en: 'She is tall and thin.', cn: '她又高又瘦。' },
+                { en: 'He is very handsome.', cn: '他非常英俊。' },
+                { en: 'The girl is very pretty.', cn: '那个女孩非常漂亮。' },
+                { en: 'The baby is so cute!', cn: '那个宝宝太可爱了！' }
+            ],
+            dialogue: [
+                { speaker: 'Xiaoling', en: 'Janet, who\'s that lady in the picture?', cn: '珍妮特，照片里那位女士是谁？' },
+                { speaker: 'Janet', en: 'Which one?', cn: '哪一个？' },
+                { speaker: 'Xiaoling', en: 'The tall and thin lady.', cn: '那个又高又瘦的女士。' },
+                { speaker: 'Janet', en: 'Oh, she\'s my aunt. My mother\'s sister.', cn: '哦，她是我阿姨。我妈妈的妹妹。' },
+                { speaker: 'Xiaoling', en: 'She\'s very pretty!', cn: '她非常漂亮！' },
+                { speaker: 'Janet', en: 'Thank you! And that man is my uncle.', cn: '谢谢！那位男士是我姨父。' },
+                { speaker: 'Xiaoling', en: 'He looks strong.', cn: '他看起来很强壮。' },
+                { speaker: 'Janet', en: 'Yes, he is. And who\'s that cute baby?', cn: '是的。那个可爱的宝宝是谁？' },
+                { speaker: 'Xiaoling', en: 'That\'s me! I was a baby then.', cn: '那是我！那时候我还是个宝宝。' },
+                { speaker: 'Janet', en: 'Wow! You were so cute!', cn: '哇！你太可爱了！' }
+            ]
+        }
+    };
+
+    if (window.practiceBank && window.practiceBank.registerModuleContent) {
+        window.practiceBank.registerModuleContent('p3u', 'm4', moduleContent);
+    }
+})();
---
+++ js/data/p5u-m1.js
@@ -0,0 +1,161 @@
+// js/data/p5u-m1.js
+// 五年级上册 Module 1 - 爱好
+// 内容数据文件（懒加载用）
+(function() {
+    'use strict';
+    
+    // 模块内容数据
+    const moduleContent = {
+        'u1': {
+                    // 单词
+                    words: [
+                        { en: 'hobby', cn: '爱好' },
+                        { en: 'collect', cn: '收集' },
+                        { en: 'stamp', cn: '邮票' },
+                        { en: 'more', cn: '更多的' },
+                        { en: 'country', cn: '国家' },
+                        { en: 'keep', cn: '保持；饲养' },
+                        { en: 'animal', cn: '动物' },
+                        { en: 'every', cn: '每一个' },
+                        { en: 'during', cn: '在……期间' },
+                        { en: 'holiday', cn: '假日；假期' },
+                        { en: 'learn', cn: '学习；学会' },
+                        { en: 'person', cn: '人' },
+                        { en: 'photo', cn: '照片' },
+                        { en: 'chess', cn: '国际象棋' },
+                        { en: 'play', cn: '玩；演奏' },
+                        { en: 'music', cn: '音乐' },
+                        { en: 'paint', cn: '用颜料画' },
+                        { en: 'read', cn: '读；阅读' },
+                        { en: 'write', cn: '写；书写' },
+                        { en: 'draw', cn: '画' }
+                    ],
+                    // 短语
+                    phrases: [
+                        { en: 'collect stamps', cn: '集邮' },
+                        { en: 'keep animals', cn: '饲养动物' },
+                        { en: 'play chess', cn: '下国际象棋' },
+                        { en: 'play music', cn: '演奏音乐' },
+                        { en: 'read books', cn: '读书' },
+                        { en: 'paint pictures', cn: '画画' },
+                        { en: 'take photos', cn: '拍照' },
+                        { en: 'during the holiday', cn: '在假期期间' },
+                        { en: 'every day', cn: '每天' },
+                        { en: 'learn from', cn: '向……学习' },
+                        { en: 'be interested in', cn: '对……感兴趣' },
+                        { en: 'lots of', cn: '许多' }
+                    ],
+                    // 句型
+                    sentences: [
+                        { en: 'What\'s your hobby?', cn: '你的爱好是什么？' },
+                        { en: 'My hobby is collecting stamps.', cn: '我的爱好是集邮。' },
+                        { en: 'I have more than 500 stamps from 20 countries.', cn: '我有来自20个国家的500多张邮票。' },
+                        { en: 'His hobby is keeping animals.', cn: '他的爱好是饲养动物。' },
+                        { en: 'What do you like doing?', cn: '你喜欢做什么？' },
+                        { en: 'I like playing chess.', cn: '我喜欢下国际象棋。' },
+                        { en: 'She is interested in music.', cn: '她对音乐感兴趣。' },
+                        { en: 'I learn a lot from books.', cn: '我从书中学到很多。' },
+                        { en: 'What do you usually do during the holiday?', cn: '你假期通常做什么？' },
+                        { en: 'I usually read books every day.', cn: '我通常每天读书。' },
+                        { en: 'That\'s a great hobby!', cn: '那是个很棒的爱好！' },
+                        { en: 'It\'s very interesting.', cn: '非常有趣。' }
+                    ],
+                    // 课文
+                    dialogue: [
+                        { en: 'Janet and Jiamin are talking about hobbies.', cn: '珍妮特和家民正在谈论爱好。' },
+                        { speaker: 'Janet', en: 'Hi, Jiamin. What\'s your hobby?', cn: '嗨，家民。你的爱好是什么？' },
+                        { speaker: 'Jiamin', en: 'My hobby is collecting stamps.', cn: '我的爱好是集邮。' },
+                        { speaker: 'Janet', en: 'Wow! That\'s great! How many stamps do you have?', cn: '哇！太棒了！你有多少张邮票？' },
+                        { speaker: 'Jiamin', en: 'I have more than 500 stamps from 20 countries.', cn: '我有来自20个国家的500多张邮票。' },
+                        { speaker: 'Janet', en: 'That\'s amazing! Where do you get them?', cn: '太神奇了！你从哪里得到它们的？' },
+                        { speaker: 'Jiamin', en: 'Some are from my letters, and some are from my friends.', cn: '有些来自我的信件，有些来自我的朋友。' },
+                        { speaker: 'Janet', en: 'What about you, Xiaoling? What\'s your hobby?', cn: '你呢，小玲？你的爱好是什么？' },
+                        { speaker: 'Xiaoling', en: 'My hobby is keeping animals.', cn: '我的爱好是饲养动物。' },
+                        { speaker: 'Jiamin', en: 'Really? What animals do you keep?', cn: '真的吗？你养什么动物？' },
+                        { speaker: 'Xiaoling', en: 'I keep two cats and a dog. They are very cute.', cn: '我养了两只猫和一只狗。它们非常可爱。' },
+                        { speaker: 'Janet', en: 'That sounds fun! I like taking photos.', cn: '听起来很有趣！我喜欢拍照。' },
+                        { speaker: 'Jiamin', en: 'Taking photos is a great hobby too.', cn: '拍照也是个很棒的爱好。' },
+                        { en: 'They all think hobbies are very interesting.', cn: '他们都认为爱好非常有趣。' }
+                    ]
+                },
+        'u2': {
+                    // 单词
+                    words: [
+                        { en: 'draw', cn: '画' },
+                        { en: 'drawing', cn: '画画；图画' },
+                        { en: 'colour', cn: '颜色；给……着色' },
+                        { en: 'picture', cn: '图片；照片' },
+                        { en: 'place', cn: '地方' },
+                        { en: 'will', cn: '将；会' },
+                        { en: 'together', cn: '一起' },
+                        { en: 'love', cn: '爱；喜爱' },
+                        { en: 'enjoy', cn: '享受；喜欢' },
+                        { en: 'free', cn: '空闲的；自由的' },
+                        { en: 'time', cn: '时间' },
+                        { en: 'when', cn: '当……的时候' },
+                        { en: 'grow', cn: '生长；种植' },
+                        { en: 'flower', cn: '花' },
+                        { en: 'garden', cn: '花园' },
+                        { en: 'plant', cn: '植物；种植' },
+                        { en: 'look', cn: '看；看起来' },
+                        { en: 'beautiful', cn: '美丽的；漂亮的' },
+                        { en: 'interesting', cn: '有趣的' },
+                        { en: 'fun', cn: '有趣的；乐趣' }
+                    ],
+                    // 短语
+                    phrases: [
+                        { en: 'draw pictures', cn: '画画' },
+                        { en: 'colour the picture', cn: '给图画着色' },
+                        { en: 'in the garden', cn: '在花园里' },
+                        { en: 'grow flowers', cn: '种花' },
+                        { en: 'plant trees', cn: '种树' },
+                        { en: 'in one\'s free time', cn: '在某人的空闲时间' },
+                        { en: 'have fun', cn: '玩得开心' },
+                        { en: 'look beautiful', cn: '看起来很漂亮' },
+                        { en: 'love doing sth.', cn: '喜爱做某事' },
+                        { en: 'enjoy doing sth.', cn: '喜欢做某事' },
+                        { en: 'every weekend', cn: '每个周末' },
+                        { en: 'after school', cn: '放学后' }
+                    ],
+                    // 句型
+                    sentences: [
+                        { en: 'His hobby is drawing.', cn: '他的爱好是画画。' },
+                        { en: 'She loves drawing very much.', cn: '她非常喜欢画画。' },
+                        { en: 'I enjoy growing flowers in the garden.', cn: '我喜欢在花园里种花。' },
+                        { en: 'What do you do in your free time?', cn: '你空闲时间做什么？' },
+                        { en: 'I usually draw pictures when I am free.', cn: '我有空的时候通常画画。' },
+                        { en: 'The flowers look very beautiful.', cn: '这些花看起来很漂亮。' },
+                        { en: 'We have lots of fun together.', cn: '我们在一起玩得很开心。' },
+                        { en: 'My grandpa plants trees every spring.', cn: '我爷爷每年春天都种树。' },
+                        { en: 'Drawing is very interesting.', cn: '画画非常有趣。' },
+                        { en: 'I will show you my pictures.', cn: '我给你看我的画。' },
+                        { en: 'Let\'s draw together.', cn: '我们一起画画吧。' },
+                        { en: 'He is good at drawing.', cn: '他擅长画画。' }
+                    ],
+                    // 课文
+                    dialogue: [
+                        { en: 'Ben and Mike are talking about their hobbies.', cn: '本和迈克正在谈论他们的爱好。' },
+                        { speaker: 'Ben', en: 'Hi, Mike. What do you usually do after school?', cn: '嗨，迈克。你放学后通常做什么？' },
+                        { speaker: 'Mike', en: 'I usually draw pictures. Drawing is my hobby.', cn: '我通常画画。画画是我的爱好。' },
+                        { speaker: 'Ben', en: 'Really? Do you enjoy drawing?', cn: '真的吗？你喜欢画画吗？' },
+                        { speaker: 'Mike', en: 'Yes, I love drawing very much.', cn: '是的，我非常喜欢画画。' },
+                        { speaker: 'Ben', en: 'What do you like drawing?', cn: '你喜欢画什么？' },
+                        { speaker: 'Mike', en: 'I like drawing animals and beautiful places.', cn: '我喜欢画动物和美丽的地方。' },
+                        { speaker: 'Ben', en: 'That sounds great! Can I see your pictures?', cn: '听起来很棒！我能看看你的画吗？' },
+                        { speaker: 'Mike', en: 'Sure! I will show you tomorrow.', cn: '当然！我明天给你看。' },
+                        { speaker: 'Ben', en: 'Thank you! My hobby is growing flowers.', cn: '谢谢你！我的爱好是种花。' },
+                        { speaker: 'Mike', en: 'Growing flowers? That\'s interesting too.', cn: '种花？那也很有趣。' },
+                        { speaker: 'Ben', en: 'Yes. I grow many flowers in my garden.', cn: '是的。我在花园里种了很多花。' },
+                        { speaker: 'Mike', en: 'Do they look beautiful?', cn: '它们看起来漂亮吗？' },
+                        { speaker: 'Ben', en: 'Yes, they look very beautiful in spring.', cn: '是的，它们春天看起来非常漂亮。' },
+                        { speaker: 'Mike', en: 'I want to see your garden too!', cn: '我也想看看你的花园！' },
+                        { en: 'Both Ben and Mike enjoy their hobbies very much.', cn: '本和迈克都非常享受他们的爱好。' }
+                    ]
+                }
+    };
+    
+    // 注册到题库
+    if (window.practiceBank && window.practiceBank.registerModuleContent) {
+        window.practiceBank.registerModuleContent('p5u', 'm1', moduleContent);
+    }
+})();
